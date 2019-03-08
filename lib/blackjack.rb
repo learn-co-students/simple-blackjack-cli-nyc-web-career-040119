6eaec374d8d68a293e75550c864a9da8dfb8c90f
Danielle Jasper <dnj547@live.com>
2019-03-08 16:15:50 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -65,8 +65,8 @@ def runner
   initial_round
   total = initial_round
   until total > 21
-    hit?(total)
+    total = hit?(total)
     display_card_total(total)
   end
-end_game
+  end_game(total)
 end
